7d52a958d6983e76515f3d7370b9b307b295bdb7
DobleTmatutin0 <zz_00024@hotmail.com>
2026-06-07 21:26:37 -0300

    sv accepts connections

--- chat-protocol/server/src/chat_sv.c
+++ chat-protocol/server/src/chat_sv.c
@@ -10,7 +10,11 @@
 #include <netinet/ip.h> // defines InternetProtocol and addess family (part of Berkeley sockets)
 #include <unistd.h> // syscalls
 #include <sys/types.h> // defines various data types
+#include <sys/select.h> // allows listening on multiple sockets
 
+
+
+#include <ClientData.h>
 
 // Server default direction.
 #define IP "127.0.0.1" // this is LocalHost IP
@@ -23,6 +27,8 @@
 
 // Server's socket file descriptor
 static int sv_sck_fd;
+
+ClientData list_of_clients[MAX_CLIENTS];
 
 
 void signHandler(int signal) {
@@ -77,8 +83,73 @@ int main(int argc, char* argv[]) {
         exit(EXIT_FAILURE);
     }
 
-    while(1) {
+    // initialize all client's sockets at 0
+    for (int i = 0; i < MAX_CLIENTS; i++) {
+        list_of_clients[i].socket_fd = 0;
+    }
 
+    // this is the mem space where are going to be saved all the file descriptos
+    // that server should "listen"
+    fd_set fds_to_listen;
+
+    
+    while(1) {
+        // cleans the mem space variable
+        FD_ZERO(&fds_to_listen);
+        
+        // add server's socket fd to the list to listen 
+        FD_SET(sv_sck_fd, &fds_to_listen);
+        
+        // esto se usa para decir cual es el file descritor mas grande que escucha el sv
+        int max_sck_fd_to_listen = sv_sck_fd;
+
+        // add clint's socket fds to the list to listen
+        for (int i = 0; i < MAX_CLIENTS; i++) {
+            // saves temporaly the client's socket fd
+            int clnt_sck_fd = list_of_clients[i].socket_fd;
+
+            if (clnt_sck_fd > 0) {
+                FD_SET(clnt_sck_fd, &fds_to_listen);
+            }
+            // updates max file descriptor to select
+            if (clnt_sck_fd > max_sck_fd_to_listen) {
+                max_sck_fd_to_listen = clnt_sck_fd;
+            }
+        }
+
+        // select recorre todos los sockets en busqueda de actividad
+        int activity = select(max_sck_fd_to_listen + 1, &fds_to_listen, NULL, NULL, NULL);
+        if (activity == -1) {
+            perror("[SERVIDOR] (ERROR) select()\n");
+            exit(EXIT_FAILURE);
+        }
+
+        // si el socket del sv tuvo actividad entonces se procede sobre la misma
+        if (FD_ISSET(sv_sck_fd, &fds_to_listen)) {
+            socklen_t address_length = sizeof(address);
+
+            // accepta conecciones
+            int new_client_socket = accept(sv_sck_fd, (struct sockaddr*) &address, &address_length);
+            if (new_client_socket == -1) {
+                perror("[SERVIDOR] (ERROR) accept()\n");
+                exit(EXIT_FAILURE);
+            }
+            
+            printf(
+                "Nueva conexión, socket fd: %d, IP: %s, Puerto: %d\n", 
+                new_client_socket, inet_ntoa(address.sin_addr), ntohs(address.sin_port)
+            );
+
+            // save the new client socket
+            for (int i = 0; i < MAX_CLIENTS; i++) {
+                if (list_of_clients[1].socket_fd == 0) {
+                    list_of_clients[1].socket_fd = new_client_socket;
+                    break;
+                }
+            }
+        }
+
+        // continuar desde aca.
     }
 }
 
